ea9b3bcf0327a8635a62699fb26efd43711c5029
dilafar <it20784720@my.sliit.lk>
2022-06-25 01:52:40 +0530

    implemented services of notice,user

---
+++ src/main/java/com/demo/demoproject/service/NoticeApi.java
@@ -0,0 +1,36 @@
+package com.demo.demoproject.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.demoproject.domain.Notice;
+import com.demo.demoproject.domain.NoticeDataAdapter;
+
+@Service
+public class NoticeApi {
+
+    private final NoticeDataAdapter noticeDataAdapter;
+
+    @Autowired
+    public NoticeApi(NoticeDataAdapter noticeDataAdapter) {
+        this.noticeDataAdapter = noticeDataAdapter;
+    }
+
+    public  List<Notice> getAll(){
+        return noticeDataAdapter.getAllNotices();
+    }
+
+    public Notice AddNotice(Notice notice){
+        return noticeDataAdapter.SaveNotice(notice);
+    }
+
+    public Notice ediNotice(Notice notice){
+        return noticeDataAdapter.UpdatNotice(notice);
+    }
+
+    public void deleteNotice(String noticeid){
+         noticeDataAdapter.DeleteNotice(noticeid);
+    }
+}
